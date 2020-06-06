a211bcd28bc991e00b9384a34f410844b9bf7be9
Алексей Зырянов <lexgp@yandex.ru>
2020-06-06 21:59:02 +0300

    Урок 20. Роуты

--- 01-first-project/react-kabzda-1/src/App.js
+++ 01-first-project/react-kabzda-1/src/App.js
@@ -4,17 +4,29 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
+import Music from './components/Music/Music'
+import News from './components/News/News'
+import Settings from './components/Settings/Settings'
+
+import {Route, BrowserRouter} from 'react-router-dom'
 
 const App = () => {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <div className="app-wrapper-content">
-        <Dialogs />
-        {/* <Profile /> */}
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+        <div className="app-wrapper-content">
+          <Route path="/dialogs" component={Dialogs}></Route>
+          <Route path="/profile" component={Profile}></Route>
+          <Route path="/news" component={News}></Route>
+          <Route path="/music" component={Music}></Route>
+          <Route path="/settings" component={Settings}></Route>
+          {/* <Dialogs /> */}
+          {/* <Profile /> */}
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
