3a2de4a4240e44adcacf4c298cf897ed352180a3
Алексей Зырянов <lexgp@yandex.ru>
2020-06-06 15:46:29 +0300

    Урок 12. Разбивка на компоненты

---
+++ 01-first-project/react-kabzda-1/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://yt3.ggpht.com/a/AATXAJy-OMUI5Js_z3cFoivPJ39WBCnEPXfEKOy6sQ=s900-c-k-c0xffffffff-no-rj-mo"></img>
+    </header>
+  )
+}
+
+export default Header
---
+++ 01-first-project/react-kabzda-1/src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://avatars.mds.yandex.net/get-pdb/28866/4d8f322c-f6d0-45ae-bf75-d694394150f6/s1200?webp=false"></img>
+      </div>
+      <div>
+        <img src="https://www.pngkey.com/png/full/202-2024792_user-profile-icon-png-download-fa-user-circle.png"></img>
+        ava + desc
+      </div>
+      <div>
+        my posts
+        <div>
+          new pos
+        </div>
+        <div>
+        <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Profile
