53a78006f4bbcbfd2b81489f4e39744964fb6caa
Алексей Зырянов <lexgp@yandex.ru>
2020-06-06 19:46:41 +0300

    Урок 16. Props

--- 01-first-project/react-kabzda-1/src/components/Profile/MyPosts/Post/Post.jsx
+++ 01-first-project/react-kabzda-1/src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import s from './Post.module.css'
 
-const Post = () => {
+const Post = (props) => {
+  const { message } = props
   return (
     <div className={s.item}>
       <img src="https://fogsoft.com/wp-content/uploads/2017/07/user-logo.png"></img>
-      post 1
+      {message}
       <div>
         <span>like</span>
       </div>
